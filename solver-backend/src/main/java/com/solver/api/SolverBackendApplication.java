631b35e3069c07112c7b1b5155159c0ca2de1f78
조현민 <hyeonmean@gmail.com>
2021-07-27 13:28:15 +0900

    [S05P12A507-100] #comment feat: Check the backend basic structure
    Former-commit-id: 7529158ca96274997cf48381b7c8bd92e6e7ea5a

---
+++ solver-backend/src/main/java/com/solver/api/SolverBackendApplication.java
@@ -0,0 +1,13 @@
+package com.solver.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolverBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolverBackendApplication.class, args);
+	}
+
+}
---
+++ solver-backend/src/test/java/com/solver/api/SolverBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solver.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolverBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
