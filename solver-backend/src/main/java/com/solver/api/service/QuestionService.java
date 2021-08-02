6a2811b1fca6b271ab6c5ef7cacd753d328dfd21
qlfflwls5 <qlfflwls2@gmail.com>
2021-08-03 00:39:08 +0900

    [S05P13A507-49] feat: complete question updating
    토큰에서 userId를 받아와 코드를 완성해야 함
    기타 question 생성, 조회 관련 코드들의 변수명 등 정리
    
    
    Former-commit-id: d1a7dd52105029cfc4205193cab16371dafdc220

--- solver-backend/src/main/java/com/solver/api/service/QuestionService.java
+++ solver-backend/src/main/java/com/solver/api/service/QuestionService.java
@@ -2,11 +2,14 @@ package com.solver.api.service;
 
 import java.util.Optional;
 
-import com.solver.api.request.QuestionReq;
+import com.solver.api.request.QuestionPatchReq;
+import com.solver.api.request.QuestionPostReq;
 import com.solver.db.entity.question.Question;
 
 public interface QuestionService {
-	Question createQuestion(QuestionReq questionReq);
+	Question createQuestion(QuestionPostReq questionPostReq);
 
 	Optional<Question> getById(String questionId);
+
+	Question updateQuestion(QuestionPatchReq questionPatchReq, Question question);
 }	
