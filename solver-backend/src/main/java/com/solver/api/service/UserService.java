1f9738530ca427c945af66de096838789c2a0ddf
조현민 <hyeonmean@gmail.com>
2021-08-02 14:41:49 +0900

    [S05P13A507-17] feat: init-backend
    Former-commit-id: ae6e5721aa544966e2909732bf9b0e616f2a8ab3

--- solver-backend/src/main/java/com/solver/api/service/UserService.java
+++ solver-backend/src/main/java/com/solver/api/service/UserService.java
@@ -2,8 +2,6 @@ package com.solver.api.service;
 
 import java.util.Optional;
 
-import com.solver.api.request.UserRegistPostReq;
-import com.solver.api.request.UserUpdatePatchReq;
 import com.solver.db.entity.user.User;
 
 public interface UserService {
--- solver-backend/src/main/java/com/solver/api/service/UserServiceImpl.java
+++ solver-backend/src/main/java/com/solver/api/service/UserServiceImpl.java
@@ -3,14 +3,9 @@ package com.solver.api.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import com.solver.api.request.UserRegistPostReq;
-import com.solver.api.request.UserUpdatePatchReq;
 import com.solver.db.entity.user.User;
-import com.solver.db.entity.user.UserCalendar;
-import com.solver.db.repository.user.UserCalendarRepository;
 import com.solver.db.repository.user.UserRepository;
 
 @Service
