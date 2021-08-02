c1dea7dad23a372aad5aad64625158cb6ad8b487
qlfflwls5 <qlfflwls2@gmail.com>
2021-08-02 17:48:43 +0900

    [S05P13A507-48] feat: complete question detail reading
    Former-commit-id: adfd79d801ccf515723028b62cea9488e6422923

--- solver-backend/src/main/java/com/solver/api/service/QuestionService.java
+++ solver-backend/src/main/java/com/solver/api/service/QuestionService.java
@@ -1,8 +1,12 @@
 package com.solver.api.service;
 
+import java.util.Optional;
+
 import com.solver.api.request.QuestionReq;
 import com.solver.db.entity.question.Question;
 
 public interface QuestionService {
 	Question createQuestion(QuestionReq questionReq);
+
+	Optional<Question> getById(String questionId);
 }	
