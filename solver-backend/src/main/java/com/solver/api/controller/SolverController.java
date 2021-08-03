8ab76d7bb02a4691f400565ed42a905f857e257b
eona1301 <eona1301@gmail.com>
2021-08-04 01:52:42 +0900

    [S05P13A507-71] feat: update solver list controller
    Former-commit-id: 6ff9d1dbfb40e44549df90a0e0c0e53301741fa3

--- solver-backend/src/main/java/com/solver/api/controller/SolverController.java
+++ solver-backend/src/main/java/com/solver/api/controller/SolverController.java
@@ -1,37 +1,30 @@
 package com.solver.api.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.AccessDeniedException;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.solver.api.request.QuestionPatchReq;
-import com.solver.api.request.QuestionPostReq;
-import com.solver.api.response.QuestionRes;
-import com.solver.api.service.QuestionService;
+import com.solver.api.request.SolverGetListReq;
+import com.solver.api.response.SolverListRes;
+import com.solver.api.response.SolverRes;
 import com.solver.api.service.UserService;
-import com.solver.common.auth.KakaoUtil;
 import com.solver.common.model.BaseResponse;
-import com.solver.db.entity.question.Question;
+import com.solver.db.entity.code.Category;
+import com.solver.db.entity.code.Code;
 import com.solver.db.entity.user.User;
+import com.solver.db.repository.code.CategoryRepository;
+import com.solver.db.repository.code.CodeRepository;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-import springfox.documentation.annotations.ApiIgnore;
 
 @Api(value = "답변자 API", tags = { "Solver" })
 @RestController
@@ -39,16 +32,42 @@ import springfox.documentation.annotations.ApiIgnore;
 public class SolverController {
 	@Autowired
 	UserService userService;
+	
+	@Autowired
+	CodeRepository codeRepository;
+	
+	@Autowired
+	CategoryRepository categoryRepository;
 
-	@PostMapping()
+
+	@GetMapping()
 	@ApiOperation(value = "답변자 목록", notes = "원하는 답변자를 찾아오기")
 	@ApiResponses({ 
 			@ApiResponse(code = 201, message = "정해진 조건에 알맞는 사람들을 찾아왔습니다."),
 			@ApiResponse(code = 400, message = "잘못된 접근입니다. 다시 확인해주세요.") })
 	public ResponseEntity<? extends BaseResponse> createQuestion(
-			@ModelAttribute QuestionPostReq questionPostReq) 
+			@ModelAttribute SolverGetListReq solverGetListReq) 
 	{
+		System.out.println(solverGetListReq.getMainCategory());
+		System.out.println(solverGetListReq.getSubCategory());
+		System.out.println(solverGetListReq.getQuery());
+		System.out.println(solverGetListReq.getMode());	
+
+		// 대분류, 소분류로 조회
+		Code mainCategory = codeRepository.findByCode(solverGetListReq.getMainCategory());
+		Category subCategory = categoryRepository.findBySubCategoryCode(solverGetListReq.getSubCategory());
+
+		System.out.println(mainCategory);
+		System.out.println(subCategory);
 		
-		return ResponseEntity.status(201).body(QuestionRes.of(201, "질문을 성공적으로 생성했습니다."));
+		List<SolverRes> list;
+		
+		try {
+			list = userService.getUserList(solverGetListReq);
+		} catch (Exception e) {
+			return ResponseEntity.status(201).body(SolverListRes.of(400, "잘못된 접근입니다."));
+		}
+		
+		return ResponseEntity.status(201).body(SolverListRes.of(201, "정해진 조건에 알맞는 사람들을 찾아왔습니다."));
 	}
 }
