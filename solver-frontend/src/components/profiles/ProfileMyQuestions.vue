d194d2bed8f82798beb9d17535f9978962bd9cf5
na_0_i <nayoi1oo5@gmail.com>
2021-08-06 01:45:25 +0900

    [S05P13A507-143] feat: profile-myquestions fix
    Former-commit-id: 5434fad77fbd040f31f61f0bae605a8f9d51ff2b

--- solver-frontend/src/components/profiles/ProfileMyQuestions.vue
+++ solver-frontend/src/components/profiles/ProfileMyQuestions.vue
@@ -1,6 +1,25 @@
 <template>
   <div>
-
+    <!-- 질문 목록 소제목 -->
+    <div style="display:flex; align-items:center; ">
+      <div class="small-box"></div>
+      <span class="subheading interval">질문 목록</span>
+    </div>
+    <!-- 질문한 질문 제목들 -->
+    <div>
+      <div class="history-content">답변했던 질문 제목</div>
+      <div class="history-content">답변했던 질문 제목 답변했던 질문 제목 답변했던 질문 제목</div>
+      <div class="history-content">답변했던 질문 제목 답변했던 질문 제목</div>
+      <div class="history-content">답변했던 질문 제목</div>
+      <div class="history-content">답변했던 질문 제목 답변했던 질문 제목 답변했던 질문 제목</div>
+      <div class="history-content">답변했던 질문 제목 답변했던 질문 제목</div>
+      <div class="history-content">답변했던 질문 제목</div>
+      <div class="history-content">답변했던 질문 제목 답변했던 질문 제목 답변했던 질문 제목</div>
+      <div class="history-content">답변했던 질문 제목 답변했던 질문 제목</div>
+      <div class="history-content">답변했던 질문 제목</div>
+      <div class="history-content">답변했던 질문 제목 답변했던 질문 제목 답변했던 질문 제목</div>
+      <div class="history-content">답변했던 질문 제목 답변했던 질문 제목</div>                
+    </div>
   </div>
 </template>
 
