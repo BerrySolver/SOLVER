24e24a860aee328ea7f67086ee09cd0bc701edce
qlfflwls5 <qlfflwls2@gmail.com>
2021-08-06 05:00:45 +0900

    [S05P13A507-127] feat: complete question list page
    기능 및 디자인 완성
    
    
    Former-commit-id: 905b21126784408c1c298d4f150a0ff3f1737987

--- solver-frontend/src/views/questions/Questions.vue
+++ solver-frontend/src/views/questions/Questions.vue
@@ -19,11 +19,11 @@
           <div style="display: flex; justify-content: space-between;">
             <span class="info2">답변: </span> 000개
           </div>
-          <div>
+          <div class="question-category-items">
             <vs-collapse open-hover >
               <vs-collapse-item class="list-group-category" :not-arrow="true">
                 <div slot="header" @click="getAllQuestionList">
-                  전체
+                  <span>전체</span>
                 </div>
               </vs-collapse-item>
               <vs-collapse-item class="list-group-category" :not-arrow="true"
@@ -48,8 +48,15 @@
 
         <div class="question-main">
           <div class="question-query">
-            <div>
-              <span>{{request.curCategory}}</span>
+            <div class="question-query-head">
+              <span style="font-size: 20px; font-weight: 700;">{{request.curCategory}}</span>
+              <div class="question-type">
+                <span class="question-type-item" :style="{color: typeColor[0]}" @click="setType(0)">전체</span>
+                <span style="margin: 0 30px 0 30px;">|</span>
+                <span class="question-type-item" :style="{color: typeColor[1]}" @click="setType(1)">해결</span>
+                <span style="margin: 0 30px 0 30px;">|</span>
+                <span class="question-type-item" :style="{color: typeColor[2]}" @click="setType(2)">미해결</span>
+              </div>
             </div>
             <div class="form-group">
                 <input class="form-field" type="text" v-model="request.query" placeholder="궁금한 모든 것을 검색해보세요!" @keypress.enter="setQuery">
@@ -72,13 +79,51 @@
                   />
               </vs-select>
             </div>
+            <div class="question-mode">
+              <span class="question-mode-item" :style="{color: modeColor[0]}" @click="setMode(0)">최신순</span>
+              <span style="margin: 0 30px 0 30px;">|</span>
+              <span class="question-mode-item" :style="{color: modeColor[1]}" @click="setMode(1)">답변순</span>
+              <span style="margin: 0 30px 0 30px;">|</span>
+              <span class="question-mode-item" :style="{color: modeColor[2]}" @click="setMode(2)">좋아요순</span>
+            </div>
           </div>
           <div class="question-list">
             <div 
+              class="question-list-item"
               v-for="(question, idx) in questionList" 
               :key="idx"
               >
-              {{question.title}}
+              <div class="d-flex" style="margin-bottom: 10px;">
+                <div style="background-color: #B5C7D3; border-radius: 6px; height: 27px; margin-top: 1px; width: 27px;">
+                  <span style="color: white; font-size: 20px; font-weight: 700;">{{question.difficulty}}</span>
+                </div>
+                <div>
+                  <span style="font-size: 20px; font-weight: 700; margin-left: 10px;">{{question.title}}</span>
+                </div>
+              </div>
+              <div class="d-flex">
+                <span style="font-size: 17px; margin-bottom: 30px;">{{question.content}}</span>
+              </div>
+              <div class="d-flex justify-content-between">
+                <div class="d-flex">
+                  <div style="padding-top: 2px;">{{question.nickname}}</div>
+                  <div style=" border-radius: 6px;  margin-left: 25px; padding: 7px 5px 0 5px; font-size: 12px;">{{humanize(now, question.regDt)}}</div>
+                </div>
+                <div>
+                  <span class="material-icons">comment</span>
+                  <span class="count">{{question.answerCount}}</span>
+                  <span class="material-icons">favorite_border</span>
+                  <span class="count">{{question.likeCount}}</span>
+                  <span class="material-icons">bookmark_border</span>
+                  <span class="count">{{question.bookMarkCount}}</span>
+                </div>
+              </div>
+            </div>
+            <div 
+              v-if="questionList.length === 0"
+              class="question-list-item"
+              >
+              <span>저쪽 신사분께 첫 질문을 남겨보시겠어요?</span>
             </div>
           </div>
         </div>
@@ -102,7 +147,7 @@ export default {
           subCategory: null,
           query: null,
           difficulty: null,
-          type: "all",
+          type: null,
           mode: "releaseDesc",
         },
         questionList: [],
@@ -111,7 +156,10 @@ export default {
           {text:'난이도 상', value: 3},
           {text:'난이도 중', value: 2},
           {text:'난이도 하', value: 1},
-        ]
+        ],
+        typeColor: ["#0F4C81", "#89848C", "#89848C"],
+        modeColor: ["#0F4C81", "#89848C", "#89848C"],
+        now: new Date()
       }
     },
     methods: {
@@ -120,7 +168,7 @@ export default {
         this.request.subCategory = null
         this.request.query = null
         this.request.difficulty = null
-        this.request.type = "all"
+        this.request.type = null,
         this.request.mode = "releaseDesc"
         this.getQuestionList()
       },
@@ -130,7 +178,7 @@ export default {
         this.request.subCategory = null
         this.request.query = null
         this.request.difficulty = null
-        this.request.type = "all"
+        this.request.type = null,
         this.request.mode = "releaseDesc"
         this.getQuestionList()
       },
@@ -140,19 +188,40 @@ export default {
         this.request.subCategory = code
         this.request.query = null
         this.request.difficulty = null
-        this.request.type = "all"
+        this.request.type = null,
         this.request.mode = "releaseDesc"
         this.getQuestionList()
       },
       setQuery: function () {
         this.request.difficulty = null
-        this.request.type = "all"
+        this.request.type = null,
         this.request.mode = "releaseDesc"
         this.getQuestionList()
       },
       setDifficulty: function () {
-        this.request.type = "all"
+        this.request.type = null,
         this.request.mode = "releaseDesc"
+        this.getQuestionList()
+      },
+      setType: function (typeNum) {
+        if (typeNum === 1) {
+          this.request.type = "040"
+        } else if (typeNum == 2) {
+          this.request.type = "041"
+        } else {
+          this.request.type = null
+        }
+        this.request.mode = "releaseDesc"
+        this.getQuestionList()
+      },
+      setMode: function (modeNum) {
+        if (modeNum === 1) {
+          this.request.mode = "answerDesc"
+        } else if (modeNum == 2) {
+          this.request.mode = "likeDesc"
+        } else {
+          this.request.mode = "releaseDesc"
+        }
         this.getQuestionList()
       },
       getQuestionList: function() {
@@ -168,7 +237,22 @@ export default {
         .catch((err) => {
           console.log(err.message)
         })
-      }
+      },
+        humanize: function (now, date) {
+        const moment = require('moment')
+        const dateData = new Date(date)
+        let r = now - dateData
+        if (parseInt(r) > 43200000) {
+          r = moment(dateData).format('YY-MM-DD\u00A0\u00A0HH:MM')
+        } else if (parseInt(r) >= 3600000) {
+          r = parseInt(parseInt(r) / 3600000).toString() + '시간 전'
+        } else if (parseInt(r) >= 60000) {
+          r = parseInt(parseInt(r) / 60000).toString() + '분 전'
+        } else {
+          r = '방금 전'
+        }
+        return r
+      },
     },
     created: function () {
       axios({
@@ -177,11 +261,51 @@ export default {
       })
       .then((res) => {
         this.categories = res.data
-        console.log(res.data)
+        this.getQuestionList()
       })
       .catch((err) => {
         console.log(err);
       });
+    },
+    computed: {
+      typeWatch: function () {
+        return this.request.type
+      },
+      modeWatch: function () {
+        return this.request.mode
+      }
+    },
+    watch: {
+      typeWatch: function () {
+        if (this.typeWatch == "040") {
+          this.typeColor[0] = "#89848C"
+          this.typeColor[1] = "#0F4C81"
+          this.typeColor[2] = "#89848C"
+        } else if (this.typeWatch == "041") {
+          this.typeColor[0] = "#89848C"
+          this.typeColor[1] = "#89848C"
+          this.typeColor[2] = "#0F4C81"
+        } else {
+          this.typeColor[0] = "#0F4C81"
+          this.typeColor[1] = "#89848C"
+          this.typeColor[2] = "#89848C"
+        }
+      },
+      modeWatch: function () {
+        if (this.modeWatch == "answerDesc") {
+          this.modeColor[0] = "#89848C"
+          this.modeColor[1] = "#0F4C81"
+          this.modeColor[2] = "#89848C"
+        } else if (this.modeWatch == "likeDesc") {
+          this.modeColor[0] = "#89848C"
+          this.modeColor[1] = "#89848C"
+          this.modeColor[2] = "#0F4C81"
+        } else {
+          this.modeColor[0] = "#0F4C81"
+          this.modeColor[1] = "#89848C"
+          this.modeColor[2] = "#89848C"
+        }
+      }
     }
 }
 </script>
@@ -217,7 +341,7 @@ export default {
 
   .form-field:focus {
     outline: none;
-    border-color: #275EFE;
+    border-color: #658DC6;
   }
 
   .form-group {
@@ -276,7 +400,7 @@ export default {
   .form-group:focus-within > span {
     color: #fff;
     background: #678EFE;
-    border-color: #275EFE;
+    border-color: #658DC6;
   }
 
   .info1 {
@@ -345,32 +469,20 @@ export default {
   }
 
   .question-body-contents {
-    border: 1px solid gold;
     display: flex;
     margin-top: 20px;
     width: 1190px;
   }
 
   .question-category {
-    border: 1px solid black;
     color: #84898C;
     text-align: left;
     width: 180px;
   }
 
-  .question-list {
-    border: 1px solid black;
-  }
-
-  .question-main{
-    border: 1px solid red;
-    margin-left: auto;
-    width: 920px;
-  }
-
-  .question-query {
-    border: 1px solid black;
-    height: 200px;
+  .question-category-items {
+    border: 1px solid #B5C7D3;
+    margin-top: 20px;
   }
 
   .question-header {
@@ -378,6 +490,61 @@ export default {
     display: flex;
     height: 180px;
     justify-content: center;
+  }
+
+  .question-list {
+    margin-bottom: 50px;
+  }
+
+  .question-list-item {
+    border-top: 1px solid #B5C7D3;
+    padding: 15px 15px 15px 15px;
+  }
+
+  .question-list-item .material-icons {
+    font-size: 21px;
+    margin: 0 6px 0 10px;
+  }
+
+  .question-list-item .count {
+    font-size: 16px;
+  }
+
+  .question-main{
+    margin-left: auto;
+    width: 920px;
+  }
+
+  .question-mode {
+    color: #84898C;
+    float: left;
+    font-weight: 600;
+    margin-top: 40px;
+    padding-top: 5px;
+  }
+
+  .question-mode-item {
+    cursor: pointer;
+  }
+
+  .question-query {
+    height: 160px;
+  }
+
+  .question-query-head {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 10px;
+  }
+
+  .question-type {
+    color: #84898C;
+    font-weight: 600;
+    padding-top: 5px;
+  }
+
+  .question-type-item {
+    cursor: pointer;
   }
 
   .selectDifficulty {
