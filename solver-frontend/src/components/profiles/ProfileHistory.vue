027e5540e6b91857debe0fd5a37a0eb66fa6c914
na_0_i <nayoi1oo5@gmail.com>
2021-08-10 00:29:57 +0900

    [S05P13A507-151] feat: profile-myAnswers axios
    Former-commit-id: 369b1fb3bc36eea1e8f5d07c0e725b41b08b1b4d

--- solver-frontend/src/components/profiles/ProfileHistory.vue
+++ solver-frontend/src/components/profiles/ProfileHistory.vue
@@ -1,29 +1,43 @@
 <template>
   <div>
-    <!-- 답변 목록 소제목 -->
+    <!-- 질문 목록 소제목 -->
     <div style="display:flex; align-items:center; ">
       <div class="small-box"></div>
       <span class="subheading interval">답변 목록</span>
     </div>
-    <!-- 답변한 질문 제목들 -->
-    <div>
-      <div class="history-content">답변했던 질문 제목</div>
-      <div class="history-content">답변했던 질문 제목 답변했던 질문 제목 답변했던 질문 제목</div>
-      <div class="history-content">답변했던 질문 제목 답변했던 질문 제목</div>
-      <div class="history-content">답변했던 질문 제목</div>
-      <div class="history-content">답변했던 질문 제목 답변했던 질문 제목 답변했던 질문 제목</div>
-      <div class="history-content">답변했던 질문 제목 답변했던 질문 제목</div>
-      <div class="history-content">답변했던 질문 제목</div>
-      <div class="history-content">답변했던 질문 제목 답변했던 질문 제목 답변했던 질문 제목</div>
-      <div class="history-content">답변했던 질문 제목 답변했던 질문 제목</div>
-      <div class="history-content">답변했던 질문 제목</div>
-      <div class="history-content">답변했던 질문 제목 답변했던 질문 제목 답변했던 질문 제목</div>
-      <div class="history-content">답변했던 질문 제목 답변했던 질문 제목</div>                
+    <!-- 질문한 질문 제목들 -->
+    <div
+      v-for="answer in myAnswers.answerQuestionList"
+      :key="answer.id"
+      class="my-answer-title">
+      - {{ answer.title }} [ {{ answer.answerCount }} ]
     </div>
   </div>
 </template>
 
-z
+<script>
+import {mapActions, mapState} from 'vuex'
+
+export default {
+  name: 'ProfileMyQuestions',
+  props: ['myAnswersTab'],
+  methods: {
+    ...mapActions(['myAnswersSetting'])
+  },
+  computed: {
+  ...mapState({
+    userNickname: state => state.auth.userNickname,
+    myAnswers: state => state.profiles.myAnswers,
+    }),
+  },
+  created() {
+    const myAnswersTabInfo = {
+      userNickname: this.userNickname,
+      tabnum: this.myAnswersTab,
+    }
+    this.myAnswersSetting(myAnswersTabInfo)
+  }
+}
 </script>
 
 <style>
@@ -31,5 +45,15 @@ z
   margin-left: 15px;
   margin-top: 10px;
   text-align: left;
+}
+
+.my-answer-title {
+  text-align: left;
+  margin: 10px;
+}
+
+.my-answer-title:hover {
+  background-color: #0F4C81;
+  color: white;
 }
 </style>
