aa9ead8d7e021f47ffeb916076de47616d0d22d8
na_0_i <nayoi1oo5@gmail.com>
2021-08-09 18:36:24 +0900

    [S05P13A507-170] feat: profile-calendar axios
    Former-commit-id: 221b88c5cd0a7aaad03a9916c3c4cafd4e257957

--- solver-frontend/src/store/modules/profiles.js
+++ solver-frontend/src/store/modules/profiles.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import API from "@/API.js";
 
 const state = {
+  myQuestions: [],
   userProfileInfo: [],
   userStatistics: [],
 };
@@ -12,33 +13,52 @@ const getters = {
 };
 
 const mutations = {
+  // SET_MY_QUESTIONS: (state, myQuestions) => {
+  //   state.myQuestions = myQuestions
+  // },
   SET_USER_PROFILE: (state, userProfileInfo) => {
     state.userProfileInfo = userProfileInfo
   },
-  SET_USER_STATISTICS: (state, userStatistics) => {
-    state.userStatistics = userStatistics
-  }
+  // SET_USER_STATISTICS: (state, userStatistics) => {
+  //   state.userStatistics = userStatistics
+  // },
 };
 
 const actions = {
+  // myQuestionsSetting({commit}, nickname) {
+  //   axios({
+  //     url: API.URL + `profiles/${nickname}/2`,
+  //     method: "get",
+  //     headers: { Authorization: "Bearer " + this.getters.getAccessToken},
+  //   })
+  //   .then((res) => {
+  //     console.log(res)
+  //     commit('SET_MY_QUESTIONS', res.data)
+  //   })
+  //   .catch((err) => console.log(err))
+  // },
   profileSetting({commit}, nickname) {
-    console.log(API.URL + `profiles/${nickname}/info`)
     axios.get(API.URL + `profiles/${nickname}/info`)
     .then((res) => {
-      console.log(res)
       console.log(res.data)
       commit('SET_USER_PROFILE', res.data)
     })
     .catch((err) => console.log(err))
   },
-  statisticSetting({commit}, nickname) {
-    axios.get(API.URL + `profiles/${nickname}/1`)
-    .then((res) => {
-      console.log('res', res)
-      console.log('res.data', res.data)
-      commit('SET_USER_STATISTICS', res.data)
-    })
-  }
+  // statisticSetting({commit}, nickname, tabNum ) {
+  //   axios({
+  //     url: API.URL + `profiles/${nickname}/tab`,
+  //     method: "get",
+  //     headers: { Authorization: "Bearer " + state.accessToken},
+  //     params: {
+  //       tabNum: tabNum,
+  //     }
+  //   })
+  //   .then((res) => {
+  //     console.log('res.data', res.data)
+  //     commit('SET_USER_STATISTICS', res.data)
+  //   })
+  // },
 };
 
 export default {
