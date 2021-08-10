6dd1b917f1f22d3734c1a86e89f9831b89b6ba5d
eona1301 <eona1301@gmail.com>
2021-08-10 14:56:57 +0900

    [S05P13A507-176] feat: solver list api code add
    Former-commit-id: 49bdab258c60955fca20f8c19d455901709768e7

--- solver-frontend/src/API.js
+++ solver-frontend/src/API.js
@@ -12,6 +12,9 @@ export default {
     signupdata: 'user/signup/data',
 
     // 질문/답변 페이지
-    getQuestionList: 'questions/list'
+    getQuestionList: 'questions/list',
+
+    // 솔버리스트
+    getSolverList: 'solver'
   }
 }
