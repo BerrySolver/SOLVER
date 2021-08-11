25a7f06862859d92641e4e0ada5bdb45e2e626cd
qlfflwls5 <qlfflwls2@gmail.com>
2021-08-11 22:40:02 +0900

    [S05P13A507-194] feat: complete answer create synchronization
    Former-commit-id: aed258ab6861f775eb7416c7aa548c04b976c99d

--- solver-frontend/src/store/modules/questions.js
+++ solver-frontend/src/store/modules/questions.js
@@ -4,12 +4,16 @@ import router from "@/router";
 
 const state = {
   query: null,
+  answerChangeTrigger: false,
   commentCreateTrigger: false,
 };
 
 const mutations = {
   SET_QUERY: (state, query) => {
     state.query = query;
+  },
+  SET_ANSWER_CHANGE_TRIGGER: (state) => {
+    state.answerChangeTrigger = !state.answerChangeTrigger
   },
   SET_COMMENT_CREATE_TRIGGER: (state) => {
     state.commentCreateTrigger = !state.commentCreateTrigger
@@ -31,6 +35,9 @@ const actions = {
         questionId: questionId,
       },
     });
+  },
+  triggerAnswerReload({ commit }) {
+    commit("SET_ANSWER_CHANGE_TRIGGER")
   },
   triggerCommentReload({ commit }) {
     commit("SET_COMMENT_CREATE_TRIGGER")
