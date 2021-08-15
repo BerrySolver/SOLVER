db3e0ab36c025256e3c0493f43ebd17275f051d4
eona1301 <eona1301@gmail.com>
2021-08-15 18:30:48 +0900

    [S05P13A507-240] feat notification response update
    Former-commit-id: 00244f757dfee75228c75b1249489faba20ad826

---
+++ solver-backend/src/main/java/com/solver/api/response/NotificationRes.java
@@ -0,0 +1,34 @@
+package com.solver.api.response;
+
+import java.util.List;
+
+import com.solver.common.model.BaseResponse;
+import com.solver.db.entity.code.Code;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@ApiModel("NotificationResponse")
+public class NotificationRes extends BaseResponse {
+
+	@ApiModelProperty(name = "질문 ID")
+	private String questionId;
+
+	@ApiModelProperty(name = "알림 종류")
+	private String code;
+
+	@ApiModelProperty(name = "질문 제목")
+	private String title;
+
+	public static NotificationRes of(Integer statusCode, String message, NotificationRes notificationRes) {
+		NotificationRes res = notificationRes;
+		res.setStatusCode(statusCode);
+		res.setMessage(message);		
+
+		return res;
+	}
+}
